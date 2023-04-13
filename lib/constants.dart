080e1efa4fb1b89fd6f72a828b2e92c857bb3f02
jayeshbhole7 <jayubhole7@gmail.com>
2023-04-13 15:05:24 +0530

    flutter project

---
+++ lib/constants.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/size_config.dart';
+
+const kPrimaryColor = Color.fromRGBO(60, 217, 238, 1);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color.fromRGBO(170, 215, 74, 1), Color.fromRGBO(73, 202, 112, 1)],
+);
+const kSecondaryColor = Color.fromRGBO(151, 151, 151, 1);
+const kTextColor = Color.fromRGBO(135, 134, 134, 1);
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding:
+      EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: kTextColor),
+  );
+}
---
+++ lib/screens/home/components/discount_banner.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+
+class DiscountBanner extends StatelessWidget {
+  const DiscountBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(getProportionateScreenWidth(20)),
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(20),
+        vertical: getProportionateScreenWidth(15),
+      ),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 49, 166, 160),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text.rich(
+        TextSpan(
+          style: TextStyle(color: Colors.white),
+          children: [
+            TextSpan(text: "Summer Surpises are here\n"),
+            TextSpan(
+              text: "Cashback 25%",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(24),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
